6544b926f835da8c84ec9bdd2da28eae482f4fdc
AjiMulyaPurnama <aqualian99@gmail.com>
2023-02-20 14:59:20 +0700

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+{
+    "name"          : "New Addons",
+    "version"       : "1.0",
+    "author"        : "Aji Mulya",
+    "website"       : "https://www.detik.com/",
+    "category"      : "New Module",
+    "summary"       : "ini adalah addons baru",
+    "description"   : """ 
+        latihan membuat addons baru di odoo 1
+    """,
+    "depends"       : [
+        "base",
+    ],
+    "data"          : [
+
+    ],
+    "demo"          : [],
+    "test"          : [],
+    "image"         : [],
+    "qweb"          : [],
+    "css"           : [],
+    "application"   : True,
+    "installable"   : True,
+    "auto_install"  : False,
+}
